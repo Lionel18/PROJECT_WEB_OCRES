0b43940c7b25cbe220cedd433c1b340b6d7dbf75
Lionel DELPHIN <lionel.delphin@edu.ece.fr>
2020-12-14 18:09:30 +0100

    back end

--- frontend_/src/App.js
+++ frontend_/src/App.js
@@ -19,15 +19,13 @@ import { Container, Row, Col, Button } from 'reactstrap';
 function App() {
 
 
-
-
   return (
 
     <Router>
 
       <Row style={{ width: "100%" }}>
         <Media query="(min-width: 750px)">
-          <Col xs='1' >
+          <Col xs='1'>
             <div style={{ height: "100vh", position: "fixed", textAlign: "center" }}>
               <div style={{ marginTop: "180px" }}>
                 <Button onClick={() => window.location = '/'} style={{ width: "120px", margin: "10px" }}>Dashboard</Button>
--- frontend_/src/components/Meteo/Meteo.js
+++ frontend_/src/components/Meteo/Meteo.js
@@ -33,7 +33,6 @@ const Meteorologie = (props) => {
 
 
 
-    const [meteo, setMeteo] = useState(meteos[0])
 
 
     const dateBuilder = (d) => {
@@ -59,6 +58,9 @@ const Meteorologie = (props) => {
 
             <main>
 
+                <div style={{ textAlign: "center" }}>
+                    <h1 className="display-4">Widget Météorologie</h1>
+                </div>
                 <div style={{ textAlign: "center" }} className="search-box">
                     <input type="text" className="search-bar" placeholder="météo de ta ville" onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search} />
                 </div>
@@ -67,8 +69,7 @@ const Meteorologie = (props) => {
                 {(typeof weather.main != "undefined") ? (
                     <div>
 
-                        <div>
-                            <h1 className="display-4">Widget Météorologie</h1>
+                        <div style={{ textAlign: "center" }}>
                             <div className="location">{weather.name}</div>
                             <sup> {weather.sys.country} </sup>
                         </div>
@@ -82,12 +83,13 @@ const Meteorologie = (props) => {
                         <div style={{ textAlign: 'center', fontSize: "20px" }} >
                             <figcaption>
 
-                                {/*
-                                    <div className="icon">
-                                         <img  src={' http://openweathermap.org/img/wn/${weather.weather.Icon}@2x.png'}/> 
-                                    </div>
-                                    */}
 
+                                <div className="icon">
+                                    <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                                </div>
+                                <h4>{weather.weather[0].description}</h4>
+
+                                <div style={{ textAlign: "center" }}> {dateBuilder(new Date())}</div>
 
                             </figcaption>
 
@@ -98,7 +100,7 @@ const Meteorologie = (props) => {
 
 
                             <section>
-                                <div style={{ textAlign: "left" }} className="date"> {dateBuilder(new Date())}</div>
+
                                 <hr></hr>
 
                                 <div className="temps"> {Math.round(weather.main.temp)}°C </div>
@@ -106,28 +108,6 @@ const Meteorologie = (props) => {
                             </section>
                         </div>
                         <hr></hr>
-
-
-                        <Container>
-
-                            <Row>
-                                {meteos.slice(1, meteos.length).map((meteo, index) => (
-                                    <Col sm="4">
-                                        <div key={'meteo' + index}>
-                                            <figcaption>
-                                                <img src={meteo.photo} width="60px" height="60px" alt="image" />
-                                            </figcaption>
-                                            <section>
-                                                <p>{meteo.date}</p>
-                                                <p>Température matin : {meteo.tempM}</p>
-                                                <p>Température après-midi : {meteo.tempAM}</p>
-                                            </section>
-                                        </div>
-                                    </Col>
-                                ))}
-
-                            </Row>
-                        </Container>
 
 
 
@@ -140,58 +120,5 @@ const Meteorologie = (props) => {
     );
 }
 
-
-
-const meteos = [{
-
-
-
-    ville: "Paris",
-    date: "29/11/2020",
-    tempM: 5,
-    tempAM: 11,
-    photo: 'https://images.squarespace-cdn.com/content/v1/5572b7b4e4b0a20071d407d4/1487091465187-9IXPUDR5TO1IBEF2CT2F/ke17ZwdGBToddI8pDm48kCMWMBFcqQftRz-JqZZoIB5Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFI99ncPZu898P4WAmVYNBp8mgB1qWbp5RirnU_Xvq-XCb8BodarTVrzIWCp72ioWw/07-Weather.png'
-
-}, {
-
-    ville: "Paris",
-    date: "30/11/2020",
-    tempM: 9,
-    tempAM: 14,
-    photo: 'https://images.squarespace-cdn.com/content/v1/5572b7b4e4b0a20071d407d4/1487091465187-9IXPUDR5TO1IBEF2CT2F/ke17ZwdGBToddI8pDm48kCMWMBFcqQftRz-JqZZoIB5Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFI99ncPZu898P4WAmVYNBp8mgB1qWbp5RirnU_Xvq-XCb8BodarTVrzIWCp72ioWw/07-Weather.png'
-
-}, {
-
-    ville: "Paris",
-    date: "01/12/2020",
-    tempM: 9,
-    tempAM: 14,
-    photo: 'https://images.squarespace-cdn.com/content/v1/5572b7b4e4b0a20071d407d4/1487091465187-9IXPUDR5TO1IBEF2CT2F/ke17ZwdGBToddI8pDm48kCMWMBFcqQftRz-JqZZoIB5Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFI99ncPZu898P4WAmVYNBp8mgB1qWbp5RirnU_Xvq-XCb8BodarTVrzIWCp72ioWw/07-Weather.png'
-
-}, {
-
-    ville: "Paris",
-    date: "02/12/2020",
-    tempM: 9,
-    tempAM: 14,
-    photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Weather-sun-thorm-shower.svg/1200px-Weather-sun-thorm-shower.svg.png'
-
-}, {
-
-    ville: "Paris",
-    date: "03/12/2020",
-    tempM: 9,
-    tempAM: 14,
-    photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Weather-sun-thorm-shower.svg/1200px-Weather-sun-thorm-shower.svg.png'
-
-}, {
-
-    ville: "Paris",
-    date: "04/12/2020",
-    tempM: 9,
-    tempAM: 11,
-    photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Weather-sun-thorm-shower.svg/1200px-Weather-sun-thorm-shower.svg.png'
-
-}]
 
 export default Meteorologie
